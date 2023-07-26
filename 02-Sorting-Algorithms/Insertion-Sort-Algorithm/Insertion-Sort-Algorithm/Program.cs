ea5e77eca7c8ef0c76b159cfb778669da2ede7a2
Furkan Eralp Kahyaoğlu <furkaneralpkahyaoglu@gmail.com>
2023-07-26 15:02:29 +0300

    Add files via upload

---
+++ 02-Sorting-Algorithms/Insertion-Sort-Algorithm/Insertion-Sort-Algorithm/Program.cs
@@ -0,0 +1,43 @@
+﻿class Sort
+{
+    public void InsertionSort(int[] A)
+    {
+        for(int i = 1; i < A.Length; i++)
+        {
+            //int[] A = { 3, 5, 8, 9, 6, 2 };
+            int temp = A[i];
+            int position = i;
+
+            while(position > 0 && A[position - 1] > temp)
+            {
+                A[position] = A[position - 1];
+                position = position - 1;
+            }
+
+            A[position] = temp;
+        }
+    }
+
+    public void Display(int[] A)
+    {
+        for(int i = 0; i < A.Length; i++)
+        {
+            Console.Write(A[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        Sort s = new Sort();
+        int[] A = { 3, 5, 8, 9, 6, 2 };
+        Console.WriteLine("Original Array: ");
+        s.Display(A);
+
+        Console.WriteLine("Sorted Array:");
+        s.InsertionSort(A);
+        s.Display(A);
+
+        Console.ReadKey();
+    }
+}
