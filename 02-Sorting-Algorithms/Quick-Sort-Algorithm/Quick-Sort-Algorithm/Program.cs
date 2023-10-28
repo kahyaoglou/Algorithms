122947ea6e3a9e2d167b3edaa52dac2bea15a45b
Furkan Eralp Kahyaoğlu <furkan.kahyaoglu@beymen.com>
2023-10-29 02:32:24 +0300

    Add files via upload

---
+++ 02-Sorting-Algorithms/Quick-Sort-Algorithm/Quick-Sort-Algorithm/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Concurrent;
+
+class Sort
+{
+    public void QuickSort(int[] A, int low, int high)
+    {
+        if(low  < high)
+        {
+            int pi = Partition(A, low, high);
+            QuickSort(A, low, pi - 1);
+            QuickSort(A, pi + 1, high);
+        }
+    }
+
+    public int Partition(int[] A, int low, int high)
+    {
+        int pivot = A[low];
+        int i = low + 1;
+        int j = high;
+        do
+        {
+            while (i <= j && A[i] <= pivot)
+            {
+                i = i + 1;
+            }
+
+            while (i <= j && A[j] > pivot)
+            {
+                j = j - 1;
+            }
+
+            if(i <= j)
+            {
+                Swap (A, i, j);
+            }
+        } while (i < j);
+        Swap(A, low, j);
+        return j;
+    }
+
+    public void Swap(int[] A, int i, int j)
+    {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
+    public void Display(int[] A, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(A[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+    static void Main(string[] args)
+    {
+        Sort s = new Sort();
+        int[] A = { 3, 5, 8, 9, 6, 2 };
+        Console.WriteLine("Original Array: ");
+        s.Display(A, 6);
+
+        Console.WriteLine("Sorted Array: ");
+        s.QuickSort(A, 0, 5);
+        s.Display(A, 6);
+        Console.ReadKey();
+    }
+}
---
+++ 02-Sorting-Algorithms/Shell-Sort-Algorithm/Shell-Sort-Algorithm/Program.cs
@@ -0,0 +1,51 @@
+﻿class Sort
+{
+    public void ShellSort(int[] A)
+    {
+        //int[] A = { 3, 5, 8, 9, 6, 2 };
+        int gap = A.Length;
+        while(gap > 0)
+        {
+            int i = gap;
+            while (i < A.Length)
+            {
+                int temp = A[i];
+                int j = i - gap;
+
+                while(j >= 0 && A[j] > temp)
+                {
+                    A[j + gap] = A[j];
+                    j = j - gap;
+                }
+
+                A[j + gap] = temp;
+                i = i + 1;
+            }
+
+            gap = gap / 2;
+        }
+    }
+
+    public void Display(int[] A)
+    {
+        for (int i = 0; i < A.Length; i++)
+        {
+            Console.Write(A[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+    static void Main(string[] args)
+    {
+        Sort s = new Sort();
+        int[] A = { 3, 5, 8, 9, 6, 2 };
+        Console.WriteLine("Original Array: ");
+        s.Display(A);
+
+        Console.WriteLine("Sorted Array: ");
+        s.ShellSort(A);
+        s.Display(A);
+        Console.ReadKey();
+    }
+}
